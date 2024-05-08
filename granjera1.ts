f74a0f5f304da5d45df9afcff1971cf7cc4afe88
gladysherrera <profherreratic@gmail.com>
2024-05-08 19:55:11 -0300

    agregué granjera y una modificacion

---
+++ granjera1.ts
@@ -0,0 +1,18 @@
+let avanzar:string = "avanzar uno";
+let cavar:string = "cavar hoyo";
+let girarI: string = "girar a la izquierda"
+console.log(avanzar);
+console.log(cavar);
+console.log(avanzar);
+console.log(cavar);
+console.log(avanzar);
+console.log(cavar);
+console.log(girarI);
+console.log(avanzar);
+console.log(cavar);
+console.log(avanzar);
+console.log(cavar);
+console.log(avanzar);
+console.log(cavar);
+
+console.log("fin");
